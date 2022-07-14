9b5b32cdfd9e38537612e04c41c7e793b2a8abdb
hongyi.li <betterlihongyi@gmail.com>
2022-07-14 21:27:06 +0800

    update

--- jxc-admin/src/main/java/com/lzj/admin/utils/AssertUtil.java
+++ jxc-admin/src/main/java/com/lzj/admin/utils/AssertUtil.java
@@ -3,7 +3,11 @@ package com.lzj.admin.utils;
 import com.lzj.admin.exceptions.ParamsException;
 
 public class AssertUtil {
-
+    /**
+     * 判断
+     * @param flag
+     * @param msg
+     */
     public static void isTrue(Boolean flag, String msg) {
         if (flag) {
             throw new ParamsException(msg);
